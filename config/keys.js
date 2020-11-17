3d25b430c99b432eafaf6c65aee8cd1d0cc757ce
fahdikrie <fahdiaf@gmail.com>
2020-11-17 10:02:15 +0700

    build: setting up initial setup for the set up

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://fahdikrie:<password>@bbbadi.gi01m.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  mongoURI: "mongodb+srv://fahdikrie:Fahdikrieboss123@bbbadi.gi01m.mongodb.net/<dbname>?retryWrites=true&w=majority"
 };
