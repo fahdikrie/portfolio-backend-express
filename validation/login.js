ed5c42e46911dd167bb52e723fa52edb5ebb4c8d
fahdikrie <fahdiaf@gmail.com>
2021-01-18 12:58:51 +0700

    fix: undo arrow function to regular ones on login & register js

--- validation/login.js
+++ validation/login.js
@@ -1,7 +1,7 @@
 const Validator = require("validator")
 const isEmpty = require("is-empty")
 
-module.exports = (data) => {
+module.exports = function validateLoginInput(data) {
   let errors = {}
 
   data.name = !isEmpty(data.name) ? data.name : ""
--- validation/register.js
+++ validation/register.js
@@ -1,7 +1,7 @@
 const Validator = require("validator")
 const isEmpty = require("is-empty")
 
-module.exports = () => {
+module.exports = function validateRegisterInput(data) {
   let errors = {};
 
   data.name = !isEmpty(data.name) ? data.name : ""
