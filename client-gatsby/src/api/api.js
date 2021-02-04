2cd8b15997f32215fe8a969e93bf1385fc08fc22
fahdikrie <fahdiaf@gmail.com>
2021-02-04 16:16:43 +0700

    build: delete unused old gatsby client folder

--- client-gatsby/src/api/api.js
+++
@@ -1,4 +0,0 @@
-export const URL = "http://localhost:8000"
-export const API_URL = "http://localhost:5000"
-// export const LOGIN_URL = `${API_URL}/api/users/login`
-export const LOGIN_URL = "http://localhost:5000/api/users/login"
